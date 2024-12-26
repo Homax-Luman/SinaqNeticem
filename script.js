4ff4b7b624338905f600d8996073f5d7537aacb8
Homax-Luman <122665117+Homax-Luman@users.noreply.github.com>
2024-12-26 23:12:08 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function checkNumber() {
   const isNomresi = document.getElementById("isNomresi").value;
   if (isNomresi.length === 7 && !isNaN(isNomresi)) {
-    window.location.href = "index.html";
+    window.location.href = "esas.html";
   } else {
     alert("Zəhmət olmasa düzgün 7 rəqəmli iş nömrəsini daxil edin!");
   }
